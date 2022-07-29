2786cdbf02a20e022dc03a2362d0613f09384a36
edubarr56@gmail.com <edubarr56@gmail.com>
2022-07-29 13:29:29 -0400

    get, post and delete method

--- src/components/FormInput.jsx
+++ src/components/FormInput.jsx
@@ -4,8 +4,15 @@ import FormLine from './FormLine';
 function FormInput (props){
     return (
         <FormLine label = {props.label}>
-            <input type={props.type} placeholder={props.placeholder} className="fullWhith" required
-             onChange={props.onChange}/>
+            <input 
+            type={props.type}
+            placeholder={props.placeholder} 
+            className="fullWhith" 
+            required
+            onChange={props.onChange}
+            value = {props.value}
+            name = {props.name}
+            onWheelCapture ={props.onWheelCapture}/>
         </FormLine>
     );
 
--- src/pages/mesa1.jsx
+++ src/pages/mesa1.jsx
@@ -1,42 +1,81 @@
-import React, {useState} from 'react';
-import FormInput from '../components/FormInput';
-import Logo from '../imagenes/Logo.jpg';
-import "./mesa.css";
+import React, {Component} from "react";
+import axios from "axios";
 
+export default class Mesa1 extends Component {
 
-function Mesa1 (){
-    const [email, setEmail] = useState();
-    const [password, setPassword] = useState();
-    const handleSubmitClick = (event) =>{
-        console.log ("Enviando" , {email, password});
+    state = {
+        registros: [],
+        form:{
+            "garzon":"",
+            "subtotal":"",
+            "pago":"",
+        },
+        error:false,
+        errorMsg:""
     }
-    return (
-    <div className = "mainlogin">
-        <div className="logo-container">
-            <img src={Logo} className="logo-login"/>
-        </div>
-        <div className = "login">
-            <h3>Inicio sesión</h3>
-            <form>
-                <FormInput label="Email" placeholder="Ingrese su Email" type="email" 
-                onChange={(event) => setEmail(event.target.value)}/>
-                <FormInput label="Contraseña" placeholder="Ingrese su contraseña" type="password" 
-                onChange={(event) => setPassword(event.target.value)}/>
-                <div className= "posicion">         
-                   <input type="submit" value="Iniciar sesión" className= "boton" onClick={handleSubmitClick} />
-                   <input type="submit" value="Registrarse" className= "boton" />
-                </div> 
-                <div className="olvido">¿Olvidaste tu contraseña?</div>
-            </form>
-        </div>
-    </div>
 
-  
-  
-  
-      
-    );
+    async componentDidMount(){
+       this.getRegistro();
+    }
 
+    getRegistro = async () =>{
+        const res = await axios.get('http://127.0.0.1:8000/mesa1');
+        this.setState ({registros: res.data})
+    }
+
+   manejadorChange = async e =>{
+    await this.setState({
+        form:{
+           ...this.state.form,
+           [e.target.name]: e.target.value 
+        }
+    })
+   }
+
+   manejadorBoton=()=>{
+    let url = 'http://127.0.0.1:8000/mesa1';
+    axios.post(url,this.state.form)
+    .then (response => {
+        console.log(response);
+    })
+   }
+
+   deleteRegistro = async (id) =>{
+    await axios.delete ('http://127.0.0.1:8000/mesa1/' + id)
+    this.getRegistro();
+   }
+
+    render(){
+        return(
+            <div >
+                <div >
+                    <h3>Nueva mesa</h3>
+                    <form>
+                        <div>nombre</div>
+                        <input  name="garzon" onChange={this.manejadorChange} />
+                        <div> subtotal </div>
+                        <input  name="subtotal" onChange={this.manejadorChange} />
+                        <div>pedido</div>
+                        <input></input>
+                        <div> pago </div>
+                        <input  name="pago" onChange={this.manejadorChange} />
+                        <button type="submit" onClick={this.manejadorBoton}> Enviar </button>
+                    </form>
+                </div>
+                <div >
+                        {this.state.registros.map(registro =>
+                        <ul key={registro.ID}>
+                            <li>Garzon: {registro.garzon}</li>
+                            <li>{registro.fecha}</li>
+                            <li>subtotal: {registro.subtotal}</li>
+                            <li >propina: {registro.propina = ((registro.subtotal * 10)/100)}</li>
+                            <li> total: {registro.total = registro.propina + registro.subtotal}</li>
+                            <button type="submit" onClick={() => this.deleteRegistro(registro.ID)}>Borrar</button>
+                        </ul>
+                        )}
+                    
+                </div>
+            </div>
+        )
+    }
 }
-
-export default Mesa1;
